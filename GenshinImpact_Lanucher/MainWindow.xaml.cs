c989f0c49aa572bbe53c5ad60fbf3b7d941c3d8b
30140 <3014085426@qq.com>
2022-05-11 01:36:29 +0800

    尝试修复Win11以下系统打开应用变成白色，但是并未实验

--- GenshinImpact_Lanucher/MainWindow.xaml.cs
+++ GenshinImpact_Lanucher/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using GenshinImpact_Lanucher.ViewModels;
+using Microsoft.VisualBasic.Devices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -33,7 +36,33 @@ namespace GenshinImpact_Lanucher
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-             WPFUI.Appearance.Watcher.Watch(this, BackgroundType.Mica, true, true);
+            var b = new ComputerInfo().OSFullName;
+            if(b.IndexOf("11") != 0)
+            {
+                //Window11
+                WPFUI.Appearance.Watcher.Watch(this, BackgroundType.Mica, true, true);
+                
+            }
+            else if(b.IndexOf("7") != 0 ||b.IndexOf("8")!=0)
+            {
+                //Windows7、Window8/Window8.1，根据WPF-UI官方文档修改
+                this.Background = new SolidColorBrush(Color.FromArgb(255, 30, 30, 30));
+                var newTheme = WPFUI.Appearance.Theme.GetAppTheme() == WPFUI.Appearance.ThemeType.Dark
+                        ? WPFUI.Appearance.ThemeType.Light
+            :               WPFUI.Appearance.ThemeType.Dark;
+                WPFUI.Appearance.Theme.Apply(
+                            themeType: ThemeType.Dark,
+                            backgroundEffect: WPFUI.Appearance.BackgroundType.Unknown,
+                            updateAccent: true,
+                            forceBackground: false);
+            }
+            else
+            {
+                //Window10
+                WPFUI.Appearance.Watcher.Watch(this, BackgroundType.Acrylic, true, true);
+                //微透明白色
+                this.Background = new SolidColorBrush(Color.FromArgb(200, 0, 0, 0));
+            }
         }
     }
 }
