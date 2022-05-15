a13578cf8735905a8fcbff0f3169588ba5bfc97c
Core Swety <swety2003@outlook.com>
2022-05-15 21:25:05 +0800

    尝试修复国际服无法启动的bug

--- GenshinImpact_Lanucher/App.xaml.cs
+++ GenshinImpact_Lanucher/App.xaml.cs
@@ -15,6 +15,31 @@ namespace GenshinImpact_Lanucher
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            // 当前作用域出现未捕获异常时，使用MyHandler函数响应事件
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+            base.OnStartup(e);
+
+
+
+
+        }
+
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            Console.WriteLine("UnHandled Exception Caught : " + e.Message);
+            Console.WriteLine("Runtime terminating: {0}", args.IsTerminating);
+
+            MessageBox.Show(e.Message + "\n" + e.StackTrace, "程序崩溃了,已生成日志：err.log！");
+            System.IO.File.WriteAllText("err.log", e.Message + e.StackTrace);
+
+        }
+
         protected override void OnExit(ExitEventArgs e)
         {
             try
